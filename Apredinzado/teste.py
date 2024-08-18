18feb11fae4d96dc3bc1feb477725440df4ce627
JamesLLP7 <jameslucaslp77@gmail.com>
2024-08-18 17:36:57 -0300

    a

--- Apredinzado/teste.py
+++ Apredinzado/teste.py
@@ -1,3 +1,9 @@
+#Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+#between 2000 and 3200 (both included).
+#the numbers obtained should be printed in a comma-separated sequence on a single line.
+
+
+
 # Initialize an empty list to store the numbers
 result = []
 
